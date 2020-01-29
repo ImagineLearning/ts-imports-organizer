14545a36fa9d9cb961368beb5c4eb3907217676d
Eric Gibby <eric.gibby@imaginelearning.com>
2020-01-29 16:13:51 -0700

    Added help message and parsing of globs for files

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+const args = require('args');
+const glob = require('glob');
+
+args.option('help', 'Shows help message').example('ts-imports-organizer files', 'Organize imports on specified files');
+
+const arguments = args.parse(process.argv, args);
+
+if (arguments.help || !arguments.sub || !arguments.sub.length) {
+	args.showHelp();
+}
+
+const files = arguments.sub.reduce((arr, sub) => {
+	const matches = glob(sub);
+	return matches && matches.length ? arr.concat(matches) : arr;
+}, []);
