fe42faa032637f7d3a6468ce16a3ffe3317bfe39
Eric Gibby <eric.gibby@imaginelearning.com>
2020-01-29 17:14:16 -0700

    Implemented imports organizing through ts-morph

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 #!/usr/bin/env node
 
-const args = require('args');
+const argv = require('argv');
 const glob = require('glob');
+const package = require('./package.json');
+const util = require('util');
+const { Project } = require('ts-morph');
 
-args.option('help', 'Shows help message').example('ts-imports-organizer files', 'Organize imports on specified files');
+const globAsync = util.promisify(glob);
 
-const arguments = args.parse(process.argv, args);
+const arguments = argv
+	.version(`v${package.version}`)
+	.option({ name: 'files', short: 'f', type: 'list,string', description: 'List of files or glob patterns of files to process' })
+	.run();
 
-if (arguments.help || !arguments.sub || !arguments.sub.length) {
-	args.showHelp();
+if (!arguments.options || arguments.options.help || !arguments.options.files || !arguments.options.files.length) {
+	argv.help();
+	process.exit(0);
 }
 
-const files = arguments.sub.reduce((arr, sub) => {
-	const matches = glob(sub);
-	return matches && matches.length ? arr.concat(matches) : arr;
-}, []);
+processFiles(arguments.options.files)
+	.then(count => {
+		console.log(`Organized imports in ${count} files`);
+		process.exit(0);
+	})
+	.catch(error => {
+		console.error(error);
+		process.exit(1);
+	});
+
+async function processFiles(globs) {
+	const matches = await Promise.all(globs.map(fileGlob => globAsync(fileGlob)));
+	const files = matches.reduce((arr, match) => (match && match.length ? arr.concat(match) : arr), []);
+
+	if (!files || !files.length) {
+		console.log('No files to process');
+		return;
+	}
+
+	const project = new Project();
+	files.forEach(file => {
+		const source = project.addSourceFileAtPath(file);
+		source.organizeImports();
+	});
+	await project.save();
+	return files.length;
+}
